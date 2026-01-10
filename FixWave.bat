b899315f757870b3af8ec16c6089d961446d6d36
RedFox <110576731+Syr0nix@users.noreply.github.com>
2026-01-10 12:48:23 -0500

    Update print statement to show 'Goodbye World'

--- FixWave.bat
+++ FixWave.bat
@@ -23,7 +23,7 @@ if "%ARG_ELEVATED%"=="0" (
 
 :: ===================== GITHUB AUTO-UPDATE =====================
 set "APP_NAME=RedFox Wave Installer"
-set "CURRENT_VER=2.0.4"
+set "CURRENT_VER=2.0.5"
 
 set "GH_USER=Syr0nix"
 set "GH_REPO=FixWave"
@@ -148,7 +148,7 @@ echo +==========================================================================
 echo ^| [1] Install Wave                                                         ^|
 echo ^| [2] Install Wave (VPS mode only works with some VPS)                     ^|
 echo ^| [3] Install Cloudflare WARP (VPN/ISP Bypass)                             ^|
-echo ^| [4] Wave Manual Inject (test)                                        ^|
+echo ^| [4] Wave Manual Inject (OutDated)                                        ^|
 echo ^| [5] Auto Fix Dependencies (Reinstalls files needed to run wave)          ^|
 echo ^| [6] Fix Desktop Path Error (OutDated)                                    ^|
 echo ^| [7] (Disabled) Defender Tweaks (not needed)                              ^|
