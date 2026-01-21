cedc4f32525621462fc5dacff734d443db25c90e
RedFox <110576731+Syr0nix@users.noreply.github.com>
2026-01-21 06:37:21 -0500

    Update FixWave.bat

--- FixWave.bat
+++ FixWave.bat
@@ -17,7 +17,7 @@ set "RAW_BAT=https://raw.githubusercontent.com/Syr0nix/FixWave/main/FixWave.bat"
 :: Get Desktop path (works even with OneDrive)
 for /f "delims=" %%D in ('powershell -NoProfile -Command "[Environment]::GetFolderPath('Desktop')"') do set "DESKTOP=%%D"
 
-set "NEWFILE=%DESKTOP%\FixWave.new.bat"
+set "NEWFILE=%DESKTOP%\FixWave.bat"
 
 :: ===================== READ LATEST VERSION =====================
 set "LATEST_VER="
@@ -817,3 +817,4 @@ echo Saved in C:\WaveSetup\Boot
 pause
 
 goto mainmenu
+
